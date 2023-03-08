99bc67eeb6ce6a1ffb7e5230315c97526b0af5a6
saurabhIR <118509671+saurabhIR@users.noreply.github.com>
2023-03-08 12:01:15 +0530

    Update send.php

--- task2/send.php
+++ task2/send.php
@@ -37,8 +37,8 @@ class EmailSender {
 		$this->mail->isSMTP();
 		$this->mail->Host = 'smtp.gmail.com';
 		$this->mail->SMTPAuth = true;
-		$this->mail->Username = 'saurabh.kumawat@innoraft.com';
-		$this->mail->Password = 'wvgzwbekgxvlnulq';
+		$this->mail->Username = 'your_email@innoraft.com';
+		$this->mail->Password = 'your_password';
 		$this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
 		$this->mail->Port = 465;
 	}
